2550b884dbb962fefc6278784106f2dc1a4b953d
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2023-05-17 18:55:59 +0500

    mpi start

---
+++ MPI/mpi/mpi.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "mpi.h"
+
+int main()
+{
+    MPI_Init(NULL, NULL);
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    printf("%d %d", rank, size);
+    MPI_Finalize();
+    return 0;
+}
+
