2ee668731f318fcfda105df3dea71a2c28eeaf03
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2023-05-18 14:47:51 +0500

    lab1mpi

--- MPI/mpi/mpi.cpp
+++ MPI/mpi/mpi.cpp
@@ -1,14 +1,16 @@
-﻿#include <iostream>
-#include "mpi.h"
+﻿#include <stdio.h>
+#include <mpi.h>
 
-int main()
-{
-    MPI_Init(NULL, NULL);
+int main(int argc, char** argv) {
     int rank, size;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    printf("%d %d", rank, size);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    printf("I am %d process from %d processes!\n", rank, size);
+    
     MPI_Finalize();
+    
     return 0;
 }
-
